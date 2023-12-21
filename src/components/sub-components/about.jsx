e6f83c0583abffda4c9fb75373e5924f65476571
ajsinp <ajaysingh98147@gmail.com>
2023-12-21 23:26:29 +0530

    about section codebase

---
+++ src/components/sub-components/about.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const About = () => {
+    return(
+        <div className="container">
+            <div className=" container flex flex-row text-white">
+                <div className="text-white"><h1 className="text-xs text-heading-color size">About</h1></div>
+                <div className="icon"></div>
+            </div>
+            <div className="flex flex-row ">
+                <div className="text-white">
+                         Scale Design <br />made in Brazil.
+                </div>
+
+            </div>
+        </div>
+    )
+};
+
+export default About;
